4defb26a55349efe518232be23a14ae4280767e2
mehmetkayprogramming <mehmetkayprogramming@linux.fritz.box>
2021-09-13 20:26:04 +0200

    Initial commit

---
+++ pomodoro/pomodoro.py
@@ -0,0 +1,15 @@
+import random
+
+# guess what x is
+input("Guess what x is" + ". "
+      + "Type a number" + ": ")
+
+
+########################################
+# create a counter function
+def counter(x):
+    if x >= int(1):
+        print(x)
+    else:
+        print("Output not possible" + ". ")
+    return 0
